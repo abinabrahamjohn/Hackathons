7e7199fec2306027e5a883438aade5daaa925f01
Abin Abraham <abinabraham.in@gmail.com>
2020-06-06 21:58:34 +0100

    day1

---
+++ AV_Janata_Hack_EmergencyVehicles/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Solution to Hackathon conducted by Analytics Vidhya. Image classification using Transfer Learning.',
+    author='Abin Abraham',
+    license='MIT',
+)
